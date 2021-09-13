8a4c61838bde2f5ec3a5d761970fb39bed0512c5
Cathyluna <keke1023136887@qq.com>
2021-09-13 17:18:16 +0800

    first initialize the project with umi

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# react_antd_blog
+# umi project
+
+## Getting Started
+
+Install dependencies,
+
+```bash
+$ yarn
+```
+
+Start the dev server,
+
+```bash
+$ yarn start
+```
---
+++ src/pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './index.less';
+import { SiderDemo } from './SiderDemo';
+
+export default function IndexPage() {
+  return (
+    <div>
+      <SiderDemo />
+    </div>
+  );
+}
